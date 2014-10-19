5fbe95d0b41bc2b2e5577f43418b9ab1166e9488
msshin <msshin@enswer.net>
2014-10-19 22:48:37 +0900

    Add package.json and others

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var browserify = require('browserify');
+
+gulp.task('default',function() {
+
+});
