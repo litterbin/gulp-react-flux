60e8c272161c634d5052d25d63542f7e9decdcf4
msshin <msshin@enswer.net>
2014-10-21 18:18:30 +0900

    Add some lib for reactjs and Fill tasks in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,54 @@
+
+// Load some modules which are installed through NPM.
 var gulp = require("gulp");
 var gutil = require('gulp-util');
+var gconnect = require('gulp-connect');
+var sass = require('gulp-sass');
+var del = require('del'); //Bundles JS.
 var source = require('vinyl-source-stream');
+var reactify = require('reactify'); // Transforms React JSX to JS.
 var watchify = require('watchify');
-var browserify = require('browserify');
+var browserify = require('browserify'); // Bundles JS.
 
-gulp.task('default',function() {
+// Define some paths
+var paths = {
+    sass: ["src/sass/*.sass"],
+    app_js: ["./src/js/app.jsx"], //TODO: ["src/js/app.jsx"] doesn't works.
+    js: ["src/js/*.js"]
+};
 
+// an example of a dependency task,it will be run before the css/js tasks.
+// Dependency tasks should call the callback to tell the parent task that
+// they're done.
+gulp.task('clean',function(done) {
+    del(["build"],done);
 });
+
+// Our CSS task. It finds all our css and compiles them.
+gulp.task('css',['clean'],function() {
+    return gulp.src(paths.sass).pipe(sass()).pipe(gulp.dest("src/css"));
+});
+
+// JS task. It will Browserify code and compile React JSX files.
+gulp.task("js",["clean"],function() {
+    //Browserify / bundle the JS
+    browserify(paths.app_js)
+        .transform(reactify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("./src/"));
+});
+
+//Rerun tasks whenever a file changes.
+gulp.task('watch',function() {
+    gulp.watch(paths.css,['css']);
+    gulp.watch(paths.js,['js']);
+});
+
+//Webserver for dev
+gulp.task('webserver',function() {
+    gconnect.server();
+});
+
+//The default task (called when we run `gulp` from cli)
+gulp.task('default',['webserver','watch','css','js']);
