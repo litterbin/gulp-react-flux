7681e19e86462173bfcbce46e0ce48c45a39945f
msshin <msshin@enswer.net>
2014-10-24 18:33:47 +0900

    Commuicate between compoents

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,40 @@
 var React = require('react');  // Browserify!
 
 var SearchBox = React.createClass({
+    getInitialState: function() {
+        return {
+            value: "Javascript rules..."
+        }
+    },
+    onInputChange: function(e) {
+        this.setState({value: e.target.value});
+        this.props.onChange(e);
+    },
     render: function() {
         return (
             <div>
             <p>Search Box</p>
-            <input />
+            <input placeholder={this.props.defaultText} value={this.state.value} onChange={this.onInputChange} />
             </div>
         );
     }
 });
 
-React.renderComponent(
-        new SearchBox({}), 
+var globalValue = "";
+
+var render = function() {
+    React.renderComponent(
+        new React.DOM.div({},
+            new SearchBox({
+                defaultText: "my text",
+                onChange: function(e) {
+                    globalValue = e.target.value;
+                    render();
+                }
+            }), 
+            new React.DOM.p({},globalValue)
+        ),
         document.body);
+}
+
+render();
