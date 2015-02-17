0d3c8e619f0b18bbea29785cb13f5d4156ab4274
msshin <anarcher@gmail.com>
2015-02-17 19:10:31 +0900

    Add tutorial01

--- hello-reflux/src/js/app.js
+++ hello-reflux/src/js/app.js
@@ -3,5 +3,47 @@
 var React = require('react');
 var Reflux = require('reflux');
 
+var Modal = require('react-modal');
 
+var appElement = document.getElementById('react');
+Modal.setAppElement(appElement);
+Modal.injectCSS();
 
+var App = React.createClass({
+    getInitialState: function() {
+        return { modalIsOpen: false };
+    },
+
+    openModal: function() {
+        this.setState({modalIsOpen: true });
+    },
+
+    closeModal: function() {
+        this.setState({modalIsOpen: false});
+    },
+
+    render: function() {
+        return (
+            <div className="all">
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                >
+                    <h2>Hello Modal</h2>
+                    <button onClick={this.closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+            </div>
+        );
+    }
+});
+
+React.render(<App />,appElement);
